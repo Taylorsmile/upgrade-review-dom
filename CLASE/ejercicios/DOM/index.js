db0afde8357f3427f56b2d3e165eedb1a43e28f1
taylorsmile <taylorabyssal@gmail.com>
2022-01-19 00:45:08 +0100

    pokedex

---
+++ CLASE/ejercicios/DOM/index.js
@@ -0,0 +1,18 @@
+let countrylist =  ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
+for (const contry of countries) {
+    let countryitem = document.createElement("ul");
+    countryitem.innerText = country;
+    country.list.appendhild(countryitem);/*da error*/
+}
+
+document.body.appendhild(countrylist);
+
+appendhild("fn-remove-me")
+
+document.body.querySelector("fn-remove-me");
+console.log.removechild("fn-remove-me")
+
+/*Utiliza el array para crear dinamicamente una lista ul > li de elementos
+en el div de html con el atributo data-function="printHere".
+const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];*/
+
